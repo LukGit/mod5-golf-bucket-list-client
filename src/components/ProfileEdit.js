283dc8ae179db701bd69ed46f9b4e8bcd20c3f9b
Shau Yin Luk <ivan.luk@icloud.com>
2020-10-15 15:02:32 -0500

    club profiles on form

--- src/components/ProfileEdit.js
+++ src/components/ProfileEdit.js
@@ -9,7 +9,31 @@ export class ProfileEdit extends Component {
     email: "",
     handicap: 0,
     club1: "",
-    club1Carry: 0
+    club1Carry: 0,
+    club2: "",
+    club2Carry: 0,
+    club3: "",
+    club3Carry: 0,
+    club4: "",
+    club4Carry: 0,
+    club5: "",
+    club5Carry: 0,
+    club6: "",
+    club6Carry: 0,
+    club7: "",
+    club7Carry: 0,
+    club8: "",
+    club8Carry: 0,
+    club9: "",
+    club9Carry: 0,
+    club10: "",
+    club10Carry: 0,
+    club11: "",
+    club11Carry: 0,
+    club12: "",
+    club12Carry: 0,
+    club13: "",
+    club13Carry: 0
   }
 
   // this is to set local state with played on date and score when component is initally loaded
@@ -19,7 +43,33 @@ export class ProfileEdit extends Component {
       return null
     } else {
       console.log(this.props.clubs.length)
-      let clubsState = (this.props.clubs.length > 0 ? {club1: this.props.clubs.club_1, club1Carry: this.props.clubs.club1_carry} : {club1: "", club1Carry: 0})
+      let clubsState = (this.props.clubs.length > 0 ?
+        {club1: this.props.clubs.club_1, club1Carry: this.props.clubs.club1_carry,
+        club2: this.props.clubs.club_2, club2Carry: this.props.clubs.club2_carry,
+        club3: this.props.clubs.club_3, club3Carry: this.props.clubs.club3_carry,
+        club4: this.props.clubs.club_4, club4Carry: this.props.clubs.club4_carry,
+        club5: this.props.clubs.club_5, club5Carry: this.props.clubs.club5_carry,
+        club6: this.props.clubs.club_6, club6Carry: this.props.clubs.club6_carry,
+        club7: this.props.clubs.club_7, club7Carry: this.props.clubs.club7_carry,
+        club8: this.props.clubs.club_8, club8Carry: this.props.clubs.club8_carry,
+        club9: this.props.clubs.club_9, club9Carry: this.props.clubs.club9_carry,
+        club10: this.props.clubs.club_10, club10Carry: this.props.clubs.club10_carry,
+        club11: this.props.clubs.club_11, club11Carry: this.props.clubs.club11_carry,
+        club12: this.props.clubs.club_12, club12Carry: this.props.clubs.club12_carry,
+        club13: this.props.clubs.club_13, club13Carry: this.props.clubs.club13_carry} 
+        : {club1: "", club1Carry: 0,
+        club2: "", club2Carry: 0,
+        club3: "", club3Carry: 0,
+        club4: "", club4Carry: 0,
+        club5: "", club5Carry: 0,
+        club6: "", club6Carry: 0,
+        club7: "", club7Carry: 0,
+        club8: "", club8Carry: 0,
+        club9: "", club9Carry: 0,
+        club10: "", club10Carry: 0,
+        club11: "", club11Carry: 0,
+        club12: "", club12Carry: 0,
+        club13: "", club13Carry: 0})
       this.setState({
         email: this.props.email,
         handicap: this.props.handicap,
@@ -59,16 +109,18 @@ export class ProfileEdit extends Component {
   }
 
   render() {
-    console.log(this.state.clubs)
+    console.log(this.state.club1, this.state.club1Carry)
     return (
       <div>
         <Navbar />
-        <Grid>
+        
+        <Grid divided='vertically'>
           <GridRow centered>
           <Segment inverted color="olive">
-          <Form onSubmit={this.handleOnSubmit} style={{width: 400}}>
-          <Label inverted color='olive'>Email Address</Label>
+          <Form onSubmit={this.handleOnSubmit} style={{width: 600}}>
+            <Form.Group widths='equal'>
             <Form.Field>
+                <lablel>Email Address</lablel>
                 <input
                   type="text"
                   name="email"
@@ -76,9 +128,8 @@ export class ProfileEdit extends Component {
                   onChange={this.handleOnChange}
                 />
             </Form.Field>
-            <Label inverted color='olive'>My Handicap</Label>
             <Form.Field>
-              
+                <lablel>My Handicap</lablel>
                 <input
                   type="text"
                   name="handicap"
@@ -86,9 +137,11 @@ export class ProfileEdit extends Component {
                   onChange={this.handleOnChange}
                 />
             </Form.Field>
-            <Segment>
-            <Label inverted color='olive'>Club 1</Label>
+            </Form.Group>
+            <Label inverted color='olive' size='large'>My Clubs Profile</Label>
+            <Form.Group widths='equal'>
               <Form.Field>
+              <lablel>Club 1</lablel>  
               <input
                   type="text"
                   name="club1"
@@ -96,16 +149,256 @@ export class ProfileEdit extends Component {
                   onChange={this.handleOnChange}
                 />
               </Form.Field>
-              <Label inverted color='olive'>Club 1 Carry</Label>
               <Form.Field>
+              <lablel>Club 1 Carry</lablel>  
               <input
                   type="text"
-                  name="club1"
+                  name="club1Carry"
                   value={this.state.club1Carry}
                   onChange={this.handleOnChange}
                 />
               </Form.Field>
-            </Segment>
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Field>
+              <lablel>Club 2</lablel>  
+              <input
+                  type="text"
+                  name="club2"
+                  value={this.state.club2}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+              <Form.Field>
+              <lablel>Club 2 Carry</lablel>  
+              <input
+                  type="text"
+                  name="club2Carry"
+                  value={this.state.club2Carry}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Field>
+              <lablel>Club 3</lablel>  
+              <input
+                  type="text"
+                  name="club3"
+                  value={this.state.club3}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+              <Form.Field>
+              <lablel>Club 3 Carry</lablel>  
+              <input
+                  type="text"
+                  name="club3Carry"
+                  value={this.state.club3Carry}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Field>
+              <lablel>Club 4</lablel>  
+              <input
+                  type="text"
+                  name="club4"
+                  value={this.state.club4}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+              <Form.Field>
+              <lablel>Club 4 Carry</lablel>  
+              <input
+                  type="text"
+                  name="club4Carry"
+                  value={this.state.club4Carry}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Field>
+              <lablel>Club 5</lablel>  
+              <input
+                  type="text"
+                  name="club5"
+                  value={this.state.club5}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+              <Form.Field>
+              <lablel>Club 5 Carry</lablel>  
+              <input
+                  type="text"
+                  name="club5Carry"
+                  value={this.state.club5Carry}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Field>
+              <lablel>Club 6</lablel>  
+              <input
+                  type="text"
+                  name="club6"
+                  value={this.state.club6}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+              <Form.Field>
+              <lablel>Club 6 Carry</lablel>  
+              <input
+                  type="text"
+                  name="club6Carry"
+                  value={this.state.club6Carry}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Field>
+              <lablel>Club 7</lablel>  
+              <input
+                  type="text"
+                  name="club7"
+                  value={this.state.club7}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+              <Form.Field>
+              <lablel>Club 7 Carry</lablel>  
+              <input
+                  type="text"
+                  name="club7Carry"
+                  value={this.state.club7Carry}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Field>
+              <lablel>Club 8</lablel>  
+              <input
+                  type="text"
+                  name="club8"
+                  value={this.state.club8}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+              <Form.Field>
+              <lablel>Club 8 Carry</lablel>  
+              <input
+                  type="text"
+                  name="club8Carry"
+                  value={this.state.club8Carry}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Field>
+              <lablel>Club 9</lablel>  
+              <input
+                  type="text"
+                  name="club9"
+                  value={this.state.club9}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+              <Form.Field>
+              <lablel>Club 9 Carry</lablel>  
+              <input
+                  type="text"
+                  name="club9Carry"
+                  value={this.state.club9Carry}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Field>
+              <lablel>Club 10</lablel>  
+              <input
+                  type="text"
+                  name="club10"
+                  value={this.state.club10}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+              <Form.Field>
+              <lablel>Club 10 Carry</lablel>  
+              <input
+                  type="text"
+                  name="club10Carry"
+                  value={this.state.club10Carry}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Field>
+              <lablel>Club 11</lablel>  
+              <input
+                  type="text"
+                  name="club11"
+                  value={this.state.club11}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+              <Form.Field>
+              <lablel>Club 11 Carry</lablel>  
+              <input
+                  type="text"
+                  name="club11Carry"
+                  value={this.state.club11Carry}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Field>
+              <lablel>Club 12</lablel>  
+              <input
+                  type="text"
+                  name="club12"
+                  value={this.state.club12}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+              <Form.Field>
+              <lablel>Club 12 Carry</lablel>  
+              <input
+                  type="text"
+                  name="club12Carry"
+                  value={this.state.club12Carry}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Field>
+              <lablel>Club 13</lablel>  
+              <input
+                  type="text"
+                  name="club13"
+                  value={this.state.club13}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+              <Form.Field>
+              <lablel>Club 13 Carry</lablel>  
+              <input
+                  type="text"
+                  name="club13Carry"
+                  value={this.state.club13Carry}
+                  onChange={this.handleOnChange}
+                />
+              </Form.Field>
+            </Form.Group>
             <Form.Field>
             <Button animated='fade' inverted color="grey" size='medium'>
               <Button.Content visible>
@@ -116,10 +409,11 @@ export class ProfileEdit extends Component {
               </Button.Content>
             </Button>
            </Form.Field>
-          </Form>
+           </Form>
           </Segment>
           </GridRow>
         </Grid>
+        
       </div>
     )
   }
